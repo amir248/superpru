2432d3639a73a118666f88f49fbfc02f084d6e44
chikchicly <chikchicly@gmail.com>
2025-02-01 18:55:20 +0700

    pay

---
+++ price.js
@@ -0,0 +1,17 @@
+async function onClicToButton(){
+    const url =
+    "https://data.fixer.io/api/fluctuation?access_key={0266c5b8751d14ff26d7b63ace6b4c33}&symbols=USD&start_date=2012-05-01&end_date=2012-05-25";
+const options = {
+    method: "GET",
+};
+
+try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+} catch (error) {
+    console.error(error);
+}
+}
+
+onClicToButton();
