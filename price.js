9c75a522e53d0354f1e82e74b752e4d086be62e1
chikchicly <chikchicly@gmail.com>
2025-02-01 19:04:44 +0700

    oK

--- price.js
+++ price.js
@@ -1,6 +1,6 @@
 async function onClicToButton(){
     const url =
-    "https://data.fixer.io/api/fluctuation?access_key={0266c5b8751d14ff26d7b63ace6b4c33}&symbols=USD&start_date=2012-05-01&end_date=2025-02-01";
+    "https://data.fixer.io/api/fluctuation?access_key={0266c5b8751d14ff26d7b63ace6b4c33}&symbols=USD&start_date=2025-01-01&end_date=2025-02-01";
 const options = {
     method: "GET",
 };
