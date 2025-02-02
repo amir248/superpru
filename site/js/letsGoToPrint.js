140203c079538f029b19e3d466b55da7f3a2cc05
chikchicly <chikchicly@gmail.com>
2025-02-02 13:32:54 +0700

    dell All

---
+++ site/js/letsGoToPrint.js
@@ -0,0 +1,39 @@
+function goToPrint(){
+    console.log('print')
+    document.querySelector('article > nav').style.display="none";
+    document.querySelector('article > fieldset').style.display="none";
+    document.querySelector('body > main > article > i').style.display="none";
+    document.querySelector('body > main > article > i').style.display="none";
+    document.querySelector('body > main > article > table').style.display="none";
+    document.querySelector('body > main > article > table').style.display="none";
+    document.querySelector('body > main > article > i').style.display="none";
+    document.querySelector('body > main > article > p').style.display="none";
+    document.querySelector('body > main > article > h2').style.display="none";
+    
+    document.querySelector('body > main > article > img').style.display="none";
+    
+    document.querySelector('body > main > article > strong').style.display="none";
+    // body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)
+    document.querySelector('body > main > article > p').style.display="none";
+    document.querySelector('body > main > article > p').style.display="none";
+    document.querySelector('body > main > article > strong').style.display="none";
+    
+    document.querySelector('body > main > article > section > strong').style.display="none";
+    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > section:nth-child(20) > section:nth-child(8)').style.display="none";
+    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > section:nth-child(20) > footer:nth-child(9)').style.display="none";
+    // document.querySelector('body > main > article > p').style.display="none";
+    // document.querySelector('body > main > article > p').style.display="none";
+    // document.querySelector('body > main > article > p').style.display="none";
+    // document.querySelector('body > main > article > strong').style.display="none";
+    // document.querySelector('body > main > article > strong').style.display="none";
+    // document.querySelector('body > main > article > strong').style.display="none";
+    // document.querySelector('body > main > article > strong').style.display="none";
+    // document.querySelector('body > main > article > strong').style.display="none";
+    // document.querySelector('body > main > article > strong').style.display="none";
+}
+document.querySelector("#letsGoToPrint").addEventListener('click',()=>{
+    goToPrint();
+})
+document.querySelector('main').addEventListener('click',()=>{
+
+});
