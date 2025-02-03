c9f5600b688f27315faeb09be9a29074617346d7
chikchicly <chikchicly@gmail.com>
2025-02-03 10:42:13 +0700

    date

--- site/js/data.js
+++ site/js/data.js
@@ -2,11 +2,18 @@ var d = new Date();
 console.log(d.getDay() + " " +d.getFullYear());
 document.querySelector('#data').innerHTML=`${d.getFullYear()}`;
 let day=d.getDay();
-document.querySelector('#dataDay').innerHTML=day;
 const month = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+
+var dateString = new Date().toLocaleString("ru-RU", { timeZone: "Asia/Tomsk" });
+var date = new Date(dateString);
+var n = date.getDay();
+document.querySelector('#data').innerHTML=dateString;
+
+console.log("Date String: ", dateString);
+console.log("Day:", n);
 
 let nameMonth = month[d.getMonth()];
 console.log(nameMonth);
 console.log(day);
-document.querySelector('#dataMonth').innerHTML=nameMonth;
-document.querySelector('#dataDay').innerHTML=day;
+// document.querySelector('#dataMonth').innerHTML=nameMonth;
+// document.querySelector('#dataDay').innerHTML=day;
