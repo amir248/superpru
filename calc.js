4a3950b4477486ffb488ae28595ab4763298dd0e
chikchicly <chikchicly@gmail.com>
2025-02-04 09:30:32 +0700

    FF

--- calc.js
+++ calc.js
@@ -1,34 +1,123 @@
 let slides='';
 document.querySelector('#slides').addEventListener('click',countPrice);
 document.querySelector('#price').addEventListener('click',countPrice);
+document.querySelector('#oneSlideo').addEventListener('click',countPrice);
+document.querySelector('#slidesEng').addEventListener('click',countPrice);
+document.querySelector('#priceEng').addEventListener('click',countPrice);
+document.querySelector('#oneSlideoEng').addEventListener('click',countPrice);
 function countPrice(){
     let value=document.querySelector('#slides').value;
-    let price=document.querySelector('#price').value;
+    let price;
+    if(document.querySelector('#oneSlideo').checked==true){
+        price=document.querySelector('#price').value;
+    }else{
+        price=0;
+    }
+    let consultation;
+    if(document.querySelector('#consultation').checked==true){
+        consultation=100;
+    }else{
+        consultation=0;
+    }
+    let seo;
+    if(document.querySelector('#seo').checked==true){
+        seo=100;
+    }else{
+        seo=0;
+    }
+    let semanticCore;
+    if(document.querySelector('#semanticCore').checked==true){
+        semanticCore=100;
+    }else{
+        semanticCore=0;
+    }
+    let google;
+    if(document.querySelector('#google').checked==true){
+        google=200;
+    }else{
+        google=0;
+    }
+    let yandex;
+    if(document.querySelector('#yandex').checked==true){
+        yandex=200;
+    }else{
+        yandex=0;
+    }
+    // --------------- ENG ----------------------------
+    let valueEng=document.querySelector('#slidesEng').value;
+    let priceEng;
+    if(document.querySelector('#oneSlideoEng').checked==true){
+        priceEng=document.querySelector('#priceEng').value;
+    }else{
+        priceEng=0;
+    }
+    let consultationEng;
+    if(document.querySelector('#consultationEng').checked==true){
+        consultationEng=100;
+    }else{
+        consultationEng=0;
+    }
+    let seoEng;
+    if(document.querySelector('#seoEng').checked==true){
+        seoEng=100;
+    }else{
+        seoEng=0;
+    }
+    let semanticCoreEng;
+    if(document.querySelector('#semanticCoreEng').checked==true){
+        semanticCoreEng=100;
+    }else{
+        semanticCoreEng=0;
+    }
+    let googleEng;
+    if(document.querySelector('#googleEng').checked==true){
+        googleEng=200;
+    }else{
+        googleEng=0;
+    }
+    let yandexEng;
+    if(document.querySelector('#yandexEng').checked==true){
+        yandexEng=200;
+    }else{
+        yandexEng=0;
+    }
+    // ___________________ ENG _____________________
+
+
     let summa=value * price;
+    let fullSumma=summa+seo+consultation+semanticCore+google+yandex;
     let oK=document.querySelector('#summa').innerHTML=`${summa}`;
-    let oKtwo=document.querySelector('#summaTwo').innerHTML=`${summa}`;
-    let totalToPay=document.querySelector('#totalToPay').innerHTML=`${summa}`;
+    let oKtwo=document.querySelector('#summaTwo').innerHTML=`${fullSumma}`;
+    let totalToPay=document.querySelector('#totalToPay').innerHTML=`${fullSumma}`;
     let oneSlice=document.querySelector('#oneSlide').innerHTML=`${price}`
     console.log(value);
     console.log(summa);
     console.log(price);
     // return oK;
-};
-countPrice();
 
-document.querySelector('#slidesEng').addEventListener('click',countPriceEng);
-document.querySelector('#priceEng').addEventListener('click',countPriceEng);
-function countPriceEng(){
-    let valueEng=document.querySelector('#slidesEng').value;
-    let priceEng=document.querySelector('#priceEng').value;
+
+
+    // ------------------------------
+    // let valueEng=document.querySelector('#slidesEng').value;
+    // let priceEng=document.querySelector('#priceEng').value;
     let summaEng=valueEng * priceEng;
+    let fullSummaEng=summaEng+seoEng+consultationEng+semanticCoreEng+googleEng+yandexEng;
     let oKeng=document.querySelector('#summaEng').innerHTML=`${summaEng}`;
-    let oKtwoEng=document.querySelector('#summaTwoEng').innerHTML=`${summaEng}`;
-    let totalToPayEng=document.querySelector('#totalToPayEng').innerHTML=`${summaEng}`;
-    let oneSliceEng=document.querySelector('#oneSlideEng').innerHTML=`${priceEng}`
+    let oKtwoEng=document.querySelector('#summaTwoEng').innerHTML=`${fullSummaEng}`;
+    let totalToPayEng=document.querySelector('#totalToPayEng').innerHTML=`${fullSummaEng}`;
+    let oneSliceEng=document.querySelector('#oneSlideoEng').innerHTML=`${fullSummaEng}`
     console.log(valueEng);
     console.log(summaEng);
     console.log(priceEng);
     // return oK;
 };
-countPriceEng();
+countPrice();
+
+// document.querySelector('#slidesEng').addEventListener('click',countPriceEng);
+// document.querySelector('#priceEng').addEventListener('click',countPriceEng);
+// document.querySelector('#oneSlideo').addEventListener('click',countPriceEng);
+    
+// function countPriceEng(){
+   
+// };
+// countPriceEng();
---
+++ site/js/additionalServices.js
@@ -0,0 +1,165 @@
+document.querySelector('#consultation').addEventListener('click',consultation);
+document.querySelector('#consultationEng').addEventListener('click',consultation);
+window.addEventListener('DOMContentLoaded',consultation);
+document.querySelector('#seo').addEventListener('click',seo);
+document.querySelector('#seoEng').addEventListener('click',seo);
+window.addEventListener('DOMContentLoaded',seo);
+document.querySelector('#semanticCore').addEventListener('click',semanticCore);
+document.querySelector('#semanticCoreEng').addEventListener('click',semanticCore);
+window.addEventListener('DOMContentLoaded',semanticCore);
+document.querySelector('#yandex').addEventListener('click',yandex);
+document.querySelector('#yandexEng').addEventListener('click',yandex);
+window.addEventListener('DOMContentLoaded',yandex);
+document.querySelector('#google').addEventListener('click',google);
+document.querySelector('#googleEng').addEventListener('click',google);
+window.addEventListener('DOMContentLoaded',google);
+document.querySelector('#oneSlideo').addEventListener('click',landingPage);
+document.querySelector('#oneSlideoEng').addEventListener('click',landingPage);
+window.addEventListener('DOMContentLoaded',landingPage);
+
+function landingPage(){
+    if(document.querySelector('#oneSlideo').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#price').removeAttribute('disabled');
+        document.querySelector('#slides').removeAttribute('disabled');
+        document.querySelector('#slides').style.background='white';
+        document.querySelector('#slides').style.color='black';
+        // document.querySelector('#price').style.display="";
+        // document.querySelector('#summa').style.display="";
+    }else if(document.querySelector('#oneSlideo').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#price').setAttribute('disabled','');
+        document.querySelector('#slides').setAttribute('disabled','');
+        document.querySelector('#slides').setAttribute('min','0');
+        document.querySelector('#slides').style.background='gray';
+        document.querySelector('#slides').style.color='gray';
+        // document.querySelector('#summa').style.display="none";
+    }else{
+        console.log('else');
+    }
+
+    if(document.querySelector('#oneSlideoEng').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#priceEng').removeAttribute('disabled');
+        document.querySelector('#summaEng').removeAttribute('disabled');
+        document.querySelector('#slidesEng').style.background='white';
+        document.querySelector('#slidesEng').style.color='black';
+    }else if(document.querySelector('#oneSlideoEng').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#priceEng').setAttribute('disabled','');
+        document.querySelector('#summaEng').setAttribute('disabled','');
+        document.querySelector('#slidesEng').style.background='gray';
+        document.querySelector('#slidesEng').style.color='gray';
+        console.log('else');
+    }
+    countPrice();
+}
+function consultation(){
+    if(document.querySelector('#consultation').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#twoSlide').style.display="";
+    }else if(document.querySelector('#consultation').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#twoSlide').style.display="none";
+    }else{
+        console.log('else');
+    }
+    if(document.querySelector('#consultationEng').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#twoSlideEng').style.display="";
+    }else if(document.querySelector('#consultationEng').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#twoSlideEng').style.display="none";
+    }else{
+        console.log('else');
+    }
+    countPrice();
+}
+
+function seo(){
+    if(document.querySelector('#seo').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#threeSlide').style.display="";
+    }else if(document.querySelector('#seo').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#threeSlide').style.display="none";
+    }else{
+        console.log('else');
+    }
+    if(document.querySelector('#seoEng').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#threeSlideEng').style.display="";
+    }else if(document.querySelector('#seoEng').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#threeSlideEng').style.display="none";
+    }else{
+        console.log('else');
+    }
+    countPrice();
+}
+
+function semanticCore(){
+    if(document.querySelector('#semanticCore').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#fourSlide').style.display="";
+    }else if(document.querySelector('#semanticCore').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#fourSlide').style.display="none";
+    }else{
+        console.log('else');
+        document.querySelector('#fourSlide').style.display="none";
+    }
+    if(document.querySelector('#semanticCoreEng').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#fourSlideEng').style.display="";
+    }else if(document.querySelector('#semanticCoreEng').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#fourSlideEng').style.display="none";
+    }else{
+        console.log('else');
+        document.querySelector('#fourSlideEng').style.display="none";
+    }
+    countPrice();
+}
+function yandex(){
+    if(document.querySelector('#yandex').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#fiveSlide').style.display="";
+    }else if(document.querySelector('#yandex').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#fiveSlide').style.display="none";
+    }else{
+        console.log('else');
+    }
+    if(document.querySelector('#yandexEng').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#fiveSlideEng').style.display="";
+    }else if(document.querySelector('#yandexEng').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#fiveSlideEng').style.display="none";
+    }else{
+        console.log('else');
+    }
+    countPrice();
+}
+function google(){
+    if(document.querySelector('#google').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#sixSlide').style.display="";
+    }else if(document.querySelector('#google').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#sixSlide').style.display="none";
+    }else{
+        console.log('else');
+    }
+    if(document.querySelector('#googleEng').checked==true){
+        console.log('chickbox Checked');
+        document.querySelector('#sixSlideEng').style.display="";
+    }else if(document.querySelector('#googleEng').checked==false){
+        console.log('checkbox -False');
+        document.querySelector('#sixSlideEng').style.display="none";
+    }else{
+        console.log('else');
+    }
+    countPrice();
+}
