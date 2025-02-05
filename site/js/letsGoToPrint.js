f8033490de56fa75fb5f5ae86a600453dc10cc00
chikchicly <chikchicly@gmail.com>
2025-02-05 08:42:20 +0700

    setAttribute

--- site/js/letsGoToPrint.js
+++ site/js/letsGoToPrint.js
@@ -3,115 +3,28 @@
         countClick++;
     if(countClick==1){
         // console.log('print')
-        document.querySelector('article > nav').style.display="none";
-        document.querySelector('article > fieldset').style.display="none";
-        document.querySelector('body > main > article > i').style.display="none";
-        document.querySelector('body > main > article > i').style.display="none";
+        document.querySelector('body > main > article > nav').style.display="none";
+        document.querySelector('body > main > article > fieldset').style.display="none";
 
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > i:nth-child(3)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > table:nth-child(5)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(8)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > h2:nth-child(9)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(15)').style.display="none";
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(18)').style.display="none";
-
-        document.querySelector('body > main > article > table').style.display="none";
-        document.querySelector('body > main > article > table').style.display="none";
-        document.querySelector('body > main > article > i').style.display="none";
-        document.querySelector('body > main > article > p').style.display="none";
-        document.querySelector('body > main > article > h2').style.display="none";
-        
-        document.querySelector('body > main > article > img').style.display="none";
-        
-        document.querySelector('body > main > article > strong').style.display="none";
-        // body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)
-        document.querySelector('body > main > article > p').style.display="none";
-        document.querySelector('body > main > article > p').style.display="none";
-        document.querySelector('body > main > article > strong').style.display="none";
-        
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)').style.display="none";
-    
-        document.querySelector('body > main > article > section > strong').style.display="none";
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > section:nth-child(20) > section:nth-child(8)').style.display="none";
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > section:nth-child(20) > footer:nth-child(9)').style.display="none";
-        // document.querySelector('body > main > article > p').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > i:nth-child(4)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > table:nth-child(6)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > i:nth-child(7)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(10)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > h2:nth-child(11)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(15)').style.display="none";
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(16)').style.display="none";
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(17)').style.display="none";
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(19)').style.display="none";
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(18)').style.display="none";
-    
-        document.querySelector('#letsGoToPrint').innerHTML=`go back`;
-
-
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(16)').style.display="none";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(17)').style.display="none";
-
+        document.querySelector('.notPrint').style.display="none";
+        document.querySelector('#invoicing > h2').style.display="none";
+        document.querySelector('#invoicing > strong:nth-child(7)').style.display="none";
         document.querySelector('#invoicing > section:nth-child(8)').style.display="none";
 
         document.querySelector('#invoicing > footer:nth-child(9)').style.display="none";
+        document.querySelector('#invoicing > footer').style.display="none";
     }else{
         // console.log('print')
-    document.querySelector('article > nav').style.display="";
-    document.querySelector('article > fieldset').style.display="";
-    document.querySelector('body > main > article > i').style.display="";
-    document.querySelector('body > main > article > i').style.display="";
+        document.querySelector('body > main > article > nav').style.display="";
+        document.querySelector('body > main > article > fieldset').style.display="";
 
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > i:nth-child(3)').style.display="";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > table:nth-child(5)').style.display="";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(8)').style.display="";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > h2:nth-child(9)').style.display="";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)').style.display="";
-        document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(15)').style.display="";
-        // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(18)').style.display="";
+        document.querySelector('.notPrint').style.display="";
+        document.querySelector('#invoicing > h2').style.display="";
+        document.querySelector('#invoicing > strong:nth-child(7)').style.display="";
+        document.querySelector('#invoicing > section:nth-child(8)').style.display="";
 
-    document.querySelector('body > main > article > table').style.display="";
-    document.querySelector('body > main > article > table').style.display="";
-    document.querySelector('body > main > article > i').style.display="";
-    document.querySelector('body > main > article > p').style.display="";
-    document.querySelector('body > main > article > h2').style.display="";
-    
-    document.querySelector('body > main > article > img').style.display="";
-    
-    document.querySelector('body > main > article > strong').style.display="";
-    // body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)
-    document.querySelector('body > main > article > p').style.display="";
-    document.querySelector('body > main > article > p').style.display="";
-    document.querySelector('body > main > article > strong').style.display="";
-    
-    // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)').style.display="none";
+        document.querySelector('#invoicing > footer').style.display="";
 
-    document.querySelector('body > main > article > section > strong').style.display="";
-    // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > section:nth-child(20) > section:nth-child(8)').style.display="";
-    // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > section:nth-child(20) > footer:nth-child(9)').style.display="";
-    // document.querySelector('body > main > article > p').style.display="none";
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > i:nth-child(4)').style.display="";
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > table:nth-child(6)').style.display="";
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > i:nth-child(7)').style.display="";
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(10)').style.display="";
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > h2:nth-child(11)').style.display="";
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(14)').style.display="";
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(15)').style.display="";
-    // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(16)').style.display="";
-    // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > p:nth-child(17)').style.display="";
-    // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(19)').style.display="";
-    // document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(18)').style.display="";
-
-    document.querySelector('#letsGoToPrint').innerHTML=`let's go to print`;
-    
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(16)').style.display="";
-    document.querySelector('body > main:nth-child(1) > article:nth-child(1) > strong:nth-child(17)').style.display="";
-
-    document.querySelector('#invoicing > footer:nth-child(9)').style.display="";
-
-    document.querySelector('#invoicing > section:nth-child(8)').style.display="";
 
     
     countClick=0;
@@ -129,12 +42,14 @@ document.querySelector("#letsGoToPrint").addEventListener('click',()=>{
 // });
 function attention(){
     let rusStrong=document.createElement('strong');
-    rusStrong.innerHTML="Чтобы распечатать счет, нажмите: \"Ctrl+P\"."
+    rusStrong.innerHTML="Чтобы распечатать счет, нажмите: \"Ctrl+P\".";
+    rusStrong.setAttribute('id','rus');
     document.querySelector('#letsGoToPrint').insertAdjacentElement('afterend',rusStrong);
     let engStrong=document.createElement('strong');
     engStrong.innerHTML="To print the invoice, press: \"Ctrl+P\".";
+    engStrong.setAttribute('id','eng');
     document.querySelector('#letsGoToPrint').insertAdjacentElement('afterend',engStrong);
-    onClickId();
+    // onClickId();
 }
 function removeAttention(){
     document.querySelector('#invoicing > div strong').remove();
