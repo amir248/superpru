0e10e2373026505e490af538be110521222b7e58
chikchicly <chikchicly@gmail.com>
2025-03-21 07:50:05 +0700

    all oK

--- site/js/data.js
+++ site/js/data.js
@@ -8,6 +8,7 @@ const month = ["01","02","03","04","05","06","07","08","09","10","11","12"];
 var dateString = new Date().toLocaleString("ru-RU", { timeZone: "Asia/Tomsk" });
 var date = new Date(dateString);
 var n = date.getDay();
+console.log(n);
 if(window.location.path=="/commercialOffer"){
     console.log('right Path');
 }
