18d68d4a2e1088deb4098b95c0dcaf80f79942ba
chikchicly <chikchicly@gmail.com>
2025-05-02 09:56:54 +0700

    you!

--- site/js/main.js
+++ site/js/main.js
@@ -4,7 +4,7 @@ function openMenu(){
         if(window.location.pathname=='/yandex/'){
             menuPath='../site/js/menu.js';
         }else if(window.location.pathname=='/yandex'){
-            menuPath='../site/js/memu.js';
+            menuPath='../site/js/menu.js';
         }else{
             menuPath='/site/js/menu.js';
         }
@@ -74,7 +74,7 @@ function linkToImg(){
             }
             resolve(toGoLink());
         }else{
-            console.log("FF");
+            // console.log("FF");
         }
     })
 };//linkToImg();
@@ -89,7 +89,7 @@ function firstImg(){
                 background:url("site/img/Header Background.jpg");
             `;
             }else{
-                console.log('=_+');
+                // console.log('=_+');
             }
         }
        
@@ -97,8 +97,43 @@ function firstImg(){
     })
     
 };
+function includeHTMLoK(){
+    return new Promise((resolve)=>{
+        function includeHTML() {
+            var z, i, elmnt, file, xhttp;
+            /* Loop through a collection of all HTML elements: */
+            z = document.getElementsByTagName("*");
+            for (i = 0; i < z.length; i++) {
+              elmnt = z[i];
+              /*search for elements with a certain atrribute:*/
+              file = elmnt.getAttribute("w3-include-html");
+              if (file) {
+                /* Make an HTTP request using the attribute value as the file name: */
+                xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {
+                  if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                  }
+                }
+                xhttp.open("GET", file, true);
+                xhttp.send();
+                /* Exit the function: */
+                return;
+              }
+            }
+          }
+          setTimeout(()=>{
+            resolve(includeHTML());
+          },1700);
+    });
+};//includeHTML
 firstImg();
 async function main(){
+    await includeHTMLoK();
     await firstImg();
     if(window.location.pathname=="/commercialOffer")await languages();
     await ifNeedCorrectPath();
