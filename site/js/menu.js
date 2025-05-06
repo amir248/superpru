f801583189d92c035a363cfb7dddd33b6025ba72
chikchicly <chikchicly@gmail.com>
2025-05-06 09:54:40 +0700

    testings

--- site/js/menu.js
+++ site/js/menu.js
@@ -1,11 +1,11 @@
-const textMenu="<h3><a href='https://madness.qucu.ru'>Home</a></h3><h3><a href='https://qucu.ru/'>qucu.ru</a></h3><h3><a href='https://nasobe.ru/'>nasobe.ru</a></h3><h3><a href='https://madness.qucu.ru/about'>about</a></h3><h3><a href='https://madness.qucu.ru/commercialOffer'>Commercial Offer</a></h3><h3><a href='https://madness.qucu.ru/goals'>Goals</a></h3><h3><a href='https://order-landing-page.qucu.ru/'>Example</a><h3><a href='https://madness.qucu.ru/commercialOffer#barter'>Barter</a></h3><h3><a href='http://madness.qucu.ru/order-professional-landing-page'>order Landing</a></h3><h3><a href='https://qucu.ru/'>qucu.ru</a></h3><h3><a href='/gallery'>Gallery</a></h3><h3><a href='https://wa.me/79528885656'>Amir Dzhan</a></h3>";
+const textMenu="<h3><a href='https://madness.qucu.ru'>Home</a></h3><h3><a href='https://qucu.ru/'>qucu.ru</a></h3><h3><a href='https://nasobe.ru/'>nasobe.ru</a></h3><h3><a href='https://madness.qucu.ru/about'>about</a></h3><h3><a href='https://madness.qucu.ru/commercialOffer'>Commercial&nbsp;Offer</a></h3><h3><a href='https://madness.qucu.ru/goals'>Goals</a></h3><h3><a href='https://order-landing-page.qucu.ru/'>Example</a><h3><a href='https://madness.qucu.ru/commercialOffer#barter'>Barter</a></h3><h3><a href='http://madness.qucu.ru/order-professional-landing-page'>order Landing</a></h3><h3><a href='https://qucu.ru/'>qucu.ru</a></h3><h3><a href='/gallery'>Gallery</a></h3><h3><a href='https://wa.me/79528885656'>Amir Dzhan</a></h3>";
 document.querySelector('#menu').addEventListener('click',()=>{
     const openMenu=document.createElement('div');
     openMenu.classList.add('openMenu');
     openMenu.innerHTML=`${textMenu}`;
     document.querySelector('body').prepend(openMenu);
     setTimeout(()=>{
-        document.querySelector('.openMenu').style.cssText=`width:100%;font-size:33px;opacity:1;z-index:99;`;
+        document.querySelector('.openMenu').style.cssText=`width:100%;font-size:20px;opacity:1;z-index:99;`;
     },300);
     closeMenu();
 });
